ce0fd460ee0202fa214bc141133731d7eda20321
Dmitry <diman_chelyad@mail.ru>
2024-07-02 20:06:41 +0300

    Добавил цикл

--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -8,46 +8,49 @@
             int number1 = 0;
             int number2 = 0;
 
-            Console.WriteLine("Введите первое число");
-            bool correctNumber1 = int.TryParse(Console.ReadLine(), out number1);
-
-            Console.WriteLine("Введите второе число");
-            bool correctNumber2 = int.TryParse(Console.ReadLine(), out number2);
-
-
-            if (correctNumber1 && correctNumber2)
+            while (true)
             {
-                Console.WriteLine("Введите операцию");
-                string typeOperation = Console.ReadLine();
+                Console.WriteLine("Введите первое число");
+                bool correctNumber1 = int.TryParse(Console.ReadLine(), out number1);
 
-                switch (typeOperation)
+                Console.WriteLine("Введите второе число");
+                bool correctNumber2 = int.TryParse(Console.ReadLine(), out number2);
+
+
+                if (correctNumber1 && correctNumber2)
                 {
-                    case "+":
-                        result = number1 + number2;
-                        break;
-                    case "-":
-                        result = number1 - number2;
-                        break;
-                    case "*":
-                        result = number1 * number2;
-                        break;
-                    case "/":
-                        result = number1 / number2;
-                        break;
-                    case "%":
-                        result = number1 % number2;
-                        break;
-                    default:
-                        Console.WriteLine("Вы ввели неправильную операция");
-                        break;
-                }
+                    Console.WriteLine("Введите операцию");
+                    string typeOperation = Console.ReadLine();
 
-                Console.Clear();
-                Console.WriteLine("Результат = " + result);
-            }
-            else
-            {
-                Console.WriteLine("Вы ввели неправильное значение");
+                    switch (typeOperation)
+                    {
+                        case "+":
+                            result = number1 + number2;
+                            break;
+                        case "-":
+                            result = number1 - number2;
+                            break;
+                        case "*":
+                            result = number1 * number2;
+                            break;
+                        case "/":
+                            result = number1 / number2;
+                            break;
+                        case "%":
+                            result = number1 % number2;
+                            break;
+                        default:
+                            Console.WriteLine("Вы ввели неправильную операция");
+                            break;
+                    }
+
+                    Console.Clear();
+                    Console.WriteLine("Результат = " + result);
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели неправильное значение");
+                }
             }
         }
     }
