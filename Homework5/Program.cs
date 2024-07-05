3752f75a42284772f236c47a25b0726e57f816b8
Dmitry <diman_chelyad@mail.ru>
2024-07-05 22:48:37 +0300

    Добавил считывание строк из файла

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -2,40 +2,18 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            //Console.WriteLine("Введите путь к файлу который нужно прочитать");
-            //string path = Console.ReadLine();
-            //path = path.Replace('\\', '/');
-            //for (int i = 0; i < path.Length; i++)
-            //{
-            //    if (path[i] == '"')
-            //    {
-            //        Console.WriteLine("Зашел");
-            //        path.Remove(i, 1);
-            //    }
-            //}
-            //Console.WriteLine(path);
-            //Console.WriteLine("hbjhvb");
+            Console.WriteLine("Введите путь к файлу который нужно прочитать");
+            string path = Console.ReadLine();
+            path = path.Replace('\\', '/');
+            path = path.Trim('"');
 
-            //path = "C:/Users/kubte/OneDrive/Рабочий стол/Telegram.txt";
-            //using (StreamReader reader = new StreamReader(path))
-            //{
-            //    string text = await reader.ReadToEndAsync();
-            //    Console.WriteLine(text);
-            //}
-
-            string str = "\"gigiggkghk\"";
-            for(int i = 0; i < args.Length; i++)
+            using (StreamReader reader = new StreamReader(path))
             {
-                if (str[i] == '\"')
-                {
-                    str.Remove(i, 1);
-                }
+                string text = await reader.ReadToEndAsync();
+                Console.WriteLine(text);
             }
-            Console.WriteLine(str);
-            str = str.Remove(0, 1);
-            Console.WriteLine(str);
         }
     }
 }
