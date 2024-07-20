c798e220608e16e90f2f735968ffe1b1279190ea
Dmitry <diman_chelyad@mail.ru>
2024-07-20 14:12:38 +0300

    Закончил 2-е задание в методичке

---
+++ Homework8/IPrintPost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8
+{
+    internal interface IPrintPost
+    {
+        void Print();
+    }
+}
---
+++ Homework8/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8
+{
+    internal class Worker : IPrintPost
+    {
+        public void Print()
+        {
+            Console.WriteLine($"Печать: Рабочий");
+        }
+    }
+}
