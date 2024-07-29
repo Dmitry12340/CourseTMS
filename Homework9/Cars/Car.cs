eb2d428f637b9e57b8699a79dc3bd8b4f4094693
Dmitry <diman_chelyad@mail.ru>
2024-07-29 17:06:42 +0300

    Сделал Homework9

---
+++ Homework9/Cars/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework9.Cars
+{
+    internal class Car
+    {
+        protected int fuel;
+        public Car(int fuel)
+        {
+            this.fuel = fuel;
+        }
+        public virtual void Drive()
+        {
+            if(fuel > 0)
+            {
+                Console.WriteLine("Автомобиль движется");
+            }
+            else
+            {
+                Console.WriteLine("Автомобиль не заправлен");
+            }
+        }
+        public virtual bool Refuel(int fuel)
+        {
+            if (fuel > 0)
+            {
+                this.fuel += fuel;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Homework9/Persons/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework9.Persons
+{
+    internal class Student : Person
+    {
+        public Student(int age, string name) : base(age, name)
+        {
+
+        }
+        public void Study()
+        {
+            Console.WriteLine("Я учусь");
+        }
+        public void ShowAge()
+        {
+            Console.WriteLine($"Мой возраст: {age} лет");
+        }
+    }
+}
