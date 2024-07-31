91ee9798b0dd46a1023a55a081fb6c0f16b2b1e2
Dmitry <diman_chelyad@mail.ru>
2024-07-31 16:51:30 +0300

    Начал выполнение 12 домашнего задания

---
+++ Homework12/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static bool Registration(string Login, string Password, string ConfirmPassword)
+        {
+            if (Login.Length > 20 || Login.Length < 3 || Login.Contains(' '))
+            {
+                throw new WrongLoginException();
+            }
+        }
+    }
+}
---
+++ Homework12/WrongLoginException.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Homework12
+{
+    [Serializable]
+    internal class WrongLoginException : Exception
+    {
+        public WrongLoginException()
+        {
+        }
+
+        public WrongLoginException(string? message) : base(message)
+        {
+        }
+
+        public WrongLoginException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
